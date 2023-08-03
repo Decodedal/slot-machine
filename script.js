336e84d56a94df4fe5d1192a6347a1502ac57889
Dallas Palumbo <palumbodallas@gmail.com>
2023-08-02 22:58:21 -0400

    slotz

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ let spinning
 spin.addEventListener('click', play)
 let controller = document.querySelectorAll('.controller')
 let bet = document.querySelector('#money')
-let bettingMoney = 0
+let bettingMoney = 100
 let total = document.querySelector('#total')
 let totalMoney = 2000
-let winnings = 0 
+let winTotal = 0 
 let winHtml = document.querySelector('#wins')
 let message = document.querySelector('#message')
+let colorVal1 = 220
+let colorVal2 = 250
 
 
 total.innerHTML = totalMoney
@@ -38,6 +40,7 @@ controller.forEach((button)=>{
 
 
 function play() {
+    if(bettingMoney ===0 )return;
     let countArr = []
     slots.forEach((slot, i) => {
         let start = Math.abs(Math.floor(Math.random() * 10 - 5))
@@ -58,36 +61,51 @@ function play() {
             countArr.push(count)
         }, 5000)
     })
+    const colorChange = setInterval(()=>{
+        colorVal1 += 10
+        colorVal2 += 20
+        document.body.style.background = `linear-gradient(hsl(${colorVal1}deg, 80%, 80%), hsl(${colorVal2}deg, 80%, 80%))`
+    },50)
     setTimeout(()=>{
         executeBet(countArr)
+        clearInterval(colorChange)
     },5001)
 
 }
 
 function executeBet(arr){
+    let won = false
+    let winnings = 0;
     if(arr.every((val => val === 2)) || arr.every((val => val === 2))){
         winnings += bettingMoney * 3
-        winHtml.innerHTML = winnings
+        won = true
         setMessage("Wow Three of a kind!")
     }
     else if(arr.every((val => val === 0)) || arr.every((val => val === 1))){
         winnings += bettingMoney * 5
-        winHtml.innerHTML = winnings
+        won = true
         setMessage("High roller Alert")
     }
     else if(arr.every((val => val === 4)) || arr.every((val => val === 5))){
         winnings += bettingMoney * 10
-        winHtml.innerHTML = winnings
+        won = true
         setMessage("JACKPOT!!!")
     }else if(arr[0] === arr[2]){
         winnings += bettingMoney * .3
-        winHtml.innerHTML = winnings
         setMessage("Nice!")
     }else if(arr[0] === arr[1] || arr[1] === arr[2]){
-        winnings += bettingMoney * .6
-        winHtml.innerHTML = winnings
+        winnings += bettingMoney * .5
+        won = true
         setMessage("two of a kind!")
     }
-     let newMoney = (totalMoney - bet) + winnings
-     total.innerHTML = newMoney
+    console.log(winnings)
+    if(won){}
+     totalMoney = (totalMoney - bettingMoney) + winnings
+     winTotal += winnings
+     winHtml.innerHTML = winTotal
+     total.innerHTML = totalMoney
+     if(totalMoney === 0)bettingMoney = 0;
+     if(bettingMoney > totalMoney)bettingMoney = totalMoney
+     bet.innerHTML = bettingMoney
 }
+
